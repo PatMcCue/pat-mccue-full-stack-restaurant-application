541723fb40a0a403fdfff197f29512afd7605ba2
Pat McCue <psmccue@gmail.com>
2024-03-07 14:38:14 -0700

    login form revert back

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { login } from '../src/components/Auth';
 import { useApp } from '../src/Providers/Context';
@@ -6,23 +7,31 @@ import { useApp } from '../src/Providers/Context';
 function Login() {
     const [data, updateData] = useState({ identifier: '', password: '' });
     const [loading, setLoading] = useState(false);
-    const [errors, setErrors] = useState({});
+    const [error, setError] = useState(false);
+    const router = useRouter();
     const { setUser, isAuthenticated, setIsAuthenticated } = useApp();
 
-    const handleChange = (event) => {
+    useEffect(() => {
+        if (isAuthenticated) {
+            router.push('/'); // redirect if you're already logged in
+        }
+    }, []);
+
+    function onChange(event) {
         updateData({ ...data, [event.target.name]: event.target.value });
-    };
+    }
 
     const handleSubmit = () => {
         setLoading(true);
         login(data.identifier, data.password)
             .then((res) => {
+                // set authed User in global context to update header/app state
                 setUser(res.data.user);
                 setIsAuthenticated(true);
                 setLoading(false);
             })
             .catch((error) => {
-                setErrors(error.response.data);
+                setError(error.response.data);
                 setLoading(false);
             });
     };
@@ -34,19 +43,27 @@ function Login() {
                     <div className="paper">
                         <div className="header"></div>
                         <section className="wrapper">
-                            {Object.keys(errors).length > 0 &&
-                                Object.values(errors).map((error) => (
-                                    <div key={error.id} style={{ marginBottom: '10px' }}>
-                                        <small style={{ color: 'red' }}>{error.message}</small>
-                                    </div>
-                                ))}
+                            {Object.entries(error).length !== 0 &&
+                                error.constructor === Object &&
+                                error.message.map((error) => {
+                                    return (
+                                        <div
+                                            key={error.messages[0].id}
+                                            style={{ marginBottom: 10 }}
+                                        >
+                                            <small style={{ color: 'red' }}>
+                                                {error.messages[0].message}
+                                            </small>
+                                        </div>
+                                    );
+                                })}
                             <Form>
                                 <fieldset disabled={loading}>
                                     <FormGroup>
                                         <Label>Email:</Label>
                                         <Input
                                             type="email"
-                                            onChange={handleChange}
+                                            onChange={(event) => onChange(event)}
                                             name="identifier"
                                             style={{ height: 50, fontSize: '1.2em' }}
                                             required
@@ -55,7 +72,7 @@ function Login() {
                                     <FormGroup style={{ marginBottom: 30 }}>
                                         <Label>Password:</Label>
                                         <Input
-                                            onChange={handleChange}
+                                            onChange={(event) => onChange(event)}
                                             type="password"
                                             name="password"
                                             style={{ height: 50, fontSize: '1.2em' }}
